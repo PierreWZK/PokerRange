4ddd6132b0e8c04616f44b302db75d793817a4c6
PierreWZK <pierredolivet@live.fr>
2024-11-06 12:02:55 +0100

    Update migration users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            // Limiter la longueur de la colonne 'email' à 191 caractères pour éviter les erreurs de longueur de clé
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
